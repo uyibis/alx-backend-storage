3e7ff74822d9f4498224e9ae941c2ca7b87456a7
Ighodaro Uyioghosa <uyibis@gmail.com>
2023-08-16 01:13:01 +0000

    updated solution

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,45 +1,23 @@
 #!/usr/bin/env python3
-"""
-Aggregation operations
-"""
+
+'''A Python module tha provides stats about nginx'''
+
 from pymongo import MongoClient
-from typing import Tuple
-
-
-def get_nginx_stats() -> Tuple:
-    """
-    Queries nginx collection for specific data
-    - Returns:
-        - count of all documents
-        - count of each method in the collection
-        - count of each GET calls to /status path
-    """
-    client: MongoClient = MongoClient()
-    db = client.logs
-    collection = db.nginx
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    method_stats = []
-    for method in methods:
-        method_count = collection.count_documents({'method': method})
-        method_stats.append({'method': method, 'count': method_count})
-    doc_count = collection.estimated_document_count()
-    status_path_stats = collection.count_documents({'method': 'GET',
-                                                    'path': '/status'})
-    client.close()
-    return doc_count, method_stats, status_path_stats
-
-
-def print_nginx_stats() -> None:
-    """
-    Prints stats from nginx query
-    """
-    doc_count, method_stats, status_path_stats = get_nginx_stats()
-    print(f'{doc_count} logs')
-    print('Methods:')
-    for method in method_stats:
-        print(f'\tmethod {method.get("method")}: {method.get("count")}')
-    print(f'{status_path_stats} status check')
 
 
 if __name__ == '__main__':
-    print_nginx_stats()
+    '''Prints the log stats in nginx collection'''
+    con = MongoClient('mongodb://localhost:27017')
+    collection = con.logs.nginx
+
+    print(f'{collection.estimated_document_count()} logs')
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('Methods:')
+
+    for req in methods:
+        print('\tmethods {}: {}'.format(req,
+              collection.count_documents({'method': req})))
+
+    print('{} status check'.format(collection.count_documents(
+          {'method': 'GET', 'path': '/status'})))
